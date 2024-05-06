358adf6ad97463d4a837f8c3ff5b87b7fd8703f3
banon <j.banon01@outlook.fr>
2024-05-06 16:29:05 +0200

    top-left-v0 done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 export default function Header () {
     return (
-        <header>
-            <div >
-                <h1 className="text-3xl font-bold underline"> Good</h1>
-                <div className="picture-container"></div>
+        <header className="h-full">
+            <div className="flex flex-row h-full">
+                <div className="basis-1/4">
+                    <div className="circle-1 rounded-full">
+                        <div className="circle-2 rounded-full"></div>
+                    </div>
+                </div>
+                <div className="basis-3/4 title-container">3/4</div>
             </div>
         </header>
     );
