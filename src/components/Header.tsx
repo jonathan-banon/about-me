c84149ce127a9f348bdc08ffbea31677b0e82f37
banon <j.banon01@outlook.fr>
2024-05-14 19:01:19 +0200

    [FEAT] - header top V0

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,9 @@ export default function Header () {
                 </div>
                 <div className="basis-3/4 title-container">
                 <div className="header-top-right-container">
-                    <p>Jonathan Banon</p>
-                    <p>Développeur React / Symfony</p>
-                    <p>Tout juste diplômé du titre RNCP concepteur développeur d'application (Bac + 3) via une alternance React / React-native d'un an, j’ai aussi eu la chance de me développer sur plusieurs technologies durant mon parcours (Symfony/Docker/MySQL). J’aime le travail en équipe et le partage de connaissances mais je suis également capable de travailler seul.</p>
+                    <p className="text-primary font-bold text-4xl">Jonathan Banon</p>
+                    <p className="text-primary font-bold text-5xl">Développeur React / Symfony</p>
+                    <p className="font-bold text-xl">Tout juste diplômé du titre RNCP concepteur développeur d'application (Bac + 3) via une alternance React / React-native d'un an, j’ai aussi eu la chance de me développer sur plusieurs technologies durant mon parcours (Symfony/Docker/MySQL). J’aime le travail en équipe et le partage de connaissances mais je suis également capable de travailler seul.</p>
                 </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#195D7E', // Remplacez cela par votre couleur primaire personnalisée
+      },
+      fontSize: {
+        'xs': '0.75rem', // Taille de police extra petite
+        'sm': '0.875rem', // Taille de police petite
+        'base': '1rem', // Taille de police de base (16px)
+        'lg': '1.125rem', // Taille de police grande
+        'xl': '1.25rem', // Taille de police très grande
+        '2xl': '1.5rem', // Taille de police encore plus grande
+        '3xl': '1.875rem', // Taille de police encore plus grande
+        '4xl': '2.25rem', // Taille de police très grande
+        '5xl': '3rem', // Taille de police extrêmement grande
+        '6xl': '4rem', // Taille de police énorme
+      },
+    },
   },
   plugins: [],
 }
