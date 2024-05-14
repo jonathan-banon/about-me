5a42bb07daba47c9bce7492acbd262ef2624d445
banon <j.banon01@outlook.fr>
2024-05-14 23:31:26 +0200

    [FEAT] - sideBar + body

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,32 @@
+export default function SideBar () {
+    return (
+        <div className="sideBar-container">
+            <div>
+                <p className="font-bold text-primary text-2xl">CONTACT</p>
+                <div>
+                    <div className="logo-mail"></div>
+                </div>
+                <div></div>
+            </div>
+            <div>
+                <p className="font-bold text-primary text-2xl">LiENS UTILES</p>
+                <div>
+                 
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <p className="font-bold text-primary text-2xl">COMPETENCES RELATIONNELLES</p>
+                <p>- Esprit déquipe</p>
+                <p>- Esprit déquipe</p>
+                <p>- Esprit déquipe</p>
+            </div>
+            <div>
+                <p className="font-bold text-primary text-2xl">LOISIRS</p>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
---
+++ src/screens/technicalSkills.tsx
@@ -0,0 +1,5 @@
+export default function technicalSkills () {
+    return (
+      <div></div>
+    );
+}
