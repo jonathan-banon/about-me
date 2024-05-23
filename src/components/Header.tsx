18cda7ea6d07a655d14cc4d44aa3d2428f943179
aio33 <jonathan.banon@aio.eu>
2024-05-23 12:58:22 +0200

    [FEAT] - adjust header size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header () {
     return (
-        <header className="h-full">
+        <header>
             <div className="flex flex-row h-full">
-                <div className="basis-1/4">
+                <div className="basis-1/4 pr-2">
                     <div className="circle-1 rounded-full">
                     </div>
                 </div>
@@ -10,7 +10,7 @@ export default function Header () {
                 <div className="header-top-right-container">
                     <p className="text-primary font-bold text-4xl">Jonathan Banon</p>
                     <p className="text-primary font-bold text-5xl">Développeur React / Symfony</p>
-                    <p className="font-bold text-xl">Tout juste diplômé du titre RNCP concepteur développeur d'application (Bac + 3) via une alternance React / React-native d'un an, j’ai aussi eu la chance de me développer sur plusieurs technologies durant mon parcours (Symfony/Docker/MySQL). J’aime le travail en équipe et le partage de connaissances mais je suis également capable de travailler seul.</p>
+                    <p className="px-3 text-l">Tout juste diplômé du titre RNCP concepteur développeur d'application (Bac + 3) via une alternance React / React-native d'un an, j’ai aussi eu la chance de me développer sur plusieurs technologies durant mon parcours (Symfony/Docker/MySQL). J’aime le travail en équipe et le partage de connaissances mais je suis également capable de travailler seul.</p>
                 </div>
                 </div>
             </div>
