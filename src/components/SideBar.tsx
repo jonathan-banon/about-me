a7ff19ce00cc4d1502acbb1f44da85fe6396c1ba
aio33 <jonathan.banon@aio.eu>
2024-05-23 12:59:10 +0200

    [FEAT] - add icons to  sideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,17 +1,31 @@
-export default function SideBar () {
+
+import { MdOutlinePhoneAndroid } from "react-icons/md";
+import { IoMailOutline } from "react-icons/io5";
+import { IconContext } from "react-icons";
+
+export default function SideBar() {
     return (
+
         <div className="sideBar-container">
             <div>
                 <p className="font-bold text-primary text-2xl">CONTACT</p>
-                <div>
-                    <div className="logo-mail"></div>
+                <div className="logo-container">
+                    <IconContext.Provider value={{ size: '30px' }}>
+                        <MdOutlinePhoneAndroid />
+                    </IconContext.Provider>
+                    <p>06 .. .. .. ..</p>
                 </div>
-                <div></div>
+                <div className="logo-container">
+                    <IconContext.Provider value={{ size: '30px' }}>
+                        <IoMailOutline />
+                    </IconContext.Provider>
+                    <p>monMail@gmail.com</p>
+                </div>
             </div>
             <div>
                 <p className="font-bold text-primary text-2xl">LiENS UTILES</p>
                 <div>
-                 
+
                 </div>
                 <div></div>
                 <div></div>
