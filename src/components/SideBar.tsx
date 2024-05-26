3416e792d0db0689e934402c02f267cb01a7a8eb
banon <j.banon01@outlook.fr>
2024-05-26 18:56:57 +0200

    [FEAT] - logo sidebar V-0

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,6 +2,10 @@
 import { MdOutlinePhoneAndroid } from "react-icons/md";
 import { IoMailOutline } from "react-icons/io5";
 import { IconContext } from "react-icons";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { PiBoxingGloveBold } from "react-icons/pi";
+import { MdOutlineSportsMotorsports } from "react-icons/md";
 
 export default function SideBar() {
     return (
@@ -25,21 +29,40 @@ export default function SideBar() {
             <div>
                 <p className="font-bold text-primary text-2xl">LiENS UTILES</p>
                 <div>
-
+                    <div className="logo-container">
+                        <IconContext.Provider value={{ size: '30px' }}>
+                            <FaGithub />
+                        </IconContext.Provider>
+                        <p>Github</p>
+                    </div>
+                    <div className="logo-container">
+                        <IconContext.Provider value={{ size: '30px' }}>
+                            <FaLinkedin />
+                        </IconContext.Provider>
+                        <p>monMail@gmail.com</p>
+                    </div>
                 </div>
-                <div></div>
-                <div></div>
             </div>
             <div>
                 <p className="font-bold text-primary text-2xl">COMPETENCES RELATIONNELLES</p>
                 <p>- Esprit déquipe</p>
-                <p>- Esprit déquipe</p>
-                <p>- Esprit déquipe</p>
+                <p>- Enthousiaste</p>
+                <p>- Adaptabilité</p>
             </div>
             <div>
                 <p className="font-bold text-primary text-2xl">LOISIRS</p>
-                <div></div>
-                <div></div>
+                <div className="logo-container">
+                    <IconContext.Provider value={{ size: '30px' }}>
+                        <PiBoxingGloveBold />
+                    </IconContext.Provider>
+                    <p>Combattant dans l'âme</p>
+                </div>
+                <div className="logo-container">
+                    <IconContext.Provider value={{ size: '30px' }}>
+                        <MdOutlineSportsMotorsports />
+                    </IconContext.Provider>
+                    <p>Bébé motard</p>
+                </div>
             </div>
         </div>
     );
