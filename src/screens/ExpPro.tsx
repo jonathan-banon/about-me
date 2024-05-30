811a6a5b371ee43dc44b56c8c11c452855338916
jonathan-banon <j.banon01@gmail.com>
2024-05-30 09:08:56 +0200

    [feat] - add content on screens

--- src/screens/ExpPro.tsx
+++ src/screens/ExpPro.tsx
@@ -1,5 +1,43 @@
-export default function ExpPro () {
+export default function ExpPro() {
     return (
-        <></>
+        <div>
+            <div>
+                <p>Sous officier dans l’armée de l'air : 2010 - 2021</p>
+                <p>Chef d’équipe ( Sous-officier supérieur )
+                    Technicien matériel servitude aéronautique
+                    Mission de sécurité intérieure ( Vigipirate / Cuirasse )</p>
+            </div>
+            <div>
+                <p>Projets Wild Code School Bordeaux : 02/21 - 07/21</p>
+                <p>Site e-commerce  B to C pour une jeune start-up (Symfony)
+                    Plateforme de jeux en simple MVC avec utilisation API REST</p>
+            </div>
+            <div>
+                <p>Plateforme e-sport (bénévolat) : 08/21 - 09/21</p>
+                <p>Migration projet MVC vers Symfony 5
+                    Intégration selon maquette</p>
+            </div>
+            <div>
+                <p>WDM studio  (stage) : 09/21 - 12/21</p>
+                <p>Intégration selon maquette
+                    Gestion API sur Symfony</p>
+            </div>
+            <div>
+                <p>Groupe QERYS (CDI) : 03/22 - 06/22</p>
+                <p>Intégration selon maquette
+                    Gestion API sur Symfony</p>
+            </div>
+            <div>
+                <p>Projet Wid Code School Bordeaux : 03/23 - 11/23</p>
+                <p>Blog interactif avec version mobile ( React / React Native )
+                    Déploiement et intégration continue ( Docker / Github Actions )</p>
+            </div>
+            <div>
+                <p>AIO (contrat pro + CDD) : 03/23 - actuellement en poste</p>
+                <p>Chef d’équipe ( Sous-officier supérieur )
+                    Technicien matériel servitude aéronautique
+                    Mission de sécurité intérieure ( Vigipirate / Cuirasse )</p>
+            </div>
+        </div>
     )
 }
--- src/screens/Skills.tsx
+++ src/screens/Skills.tsx
@@ -1,5 +1,40 @@
-export default function Skills () {
+import { IconContext } from "react-icons";
+import { FaSymfony } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { TbBrandReactNative } from "react-icons/tb";
+import { SiMysql } from "react-icons/si";
+import { GrGraphQl } from "react-icons/gr";
+import { FaVuejs } from "react-icons/fa";
+import { FaPhp } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaGitAlt } from "react-icons/fa6";
+
+export default function Skills() {
     return (
-        <></>
+        <div>
+            <IconContext.Provider value={{ size: '40px', color: 'white' }}>
+                <div>
+                    <FaVuejs />
+                    <FaSymfony />
+                    <FaReact />
+                    <TbBrandReactNative />
+                </div>
+                <div>
+                    <SiMysql />
+                    <GrGraphQl />
+                </div>
+                <div>
+                    <IoLogoJavascript />
+                    <FaPhp />
+                    <SiTypescript />
+                </div>
+                <div>
+                    <FaDocker />
+                    <FaGitAlt />
+                </div>
+            </IconContext.Provider>
+        </div>
     )
 }
