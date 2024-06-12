550e18d4303aaa35f592936bffcf0957ab94d3b3
banon <j.banon01@outlook.fr>
2024-06-12 23:43:40 +0200

    [fix] - font xl size

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         'sm': '0.875rem', // Taille de police petite
         'base': '1rem', // Taille de police de base (16px)
         'lg': '1.125rem', // Taille de police grande
-        'xl': '1.25rem', // Taille de police très grande
+        'xl': '1.20rem', // Taille de police très grande
         '2xl': '1.5rem', // Taille de police encore plus grande
         '3xl': '1.875rem', // Taille de police encore plus grande
         '4xl': '2.25rem', // Taille de police très grande
