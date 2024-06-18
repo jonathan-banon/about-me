8de7df0b9232c0bae56b0ecf187dddc2f763ba31
banon <j.banon01@outlook.fr>
2024-06-18 21:05:28 +0200

    [fix] - alignement font

--- src/screens/ExpProMobile.tsx
+++ src/screens/ExpProMobile.tsx
@@ -35,41 +35,7 @@ export default function ExpPrMobile() {
             <div className="mob-title">
                 <h1>MES EXPÉRIENCES PROFESSIONNELLES</h1>
             </div>
-            <div className='flex'>
-                <div className='text-end pr-6'>
-                    <div className=" exp-container exp-mob animate__animated" >
-                        <p className="text-sm font-bold">Sous officier dans l’armée de l'air</p>
-                        <p className='text-xs'>Chef d’équipe (Sous-officier supérieur)</p>
-                        <p className='text-xs'>Technicien matériel servitude aéronautique</p>
-                        <p className='text-xs'>Missions de sécurité intérieure (Vigipirate / Cuirasse)</p>
-                    </div>
-                    <div className=' exp-container exp-mob animate__animated' >
-                        <p className="text-sm font-bold">02/21 - 07/21</p>
-                    </div>
-                    <div className=" exp-container exp-mob animate__animated" >
-                        <p className="text-sm font-bold">Plateforme e-sport (bénévolat)</p>
-                        <p className='text-xs'>Migration projet MVC vers Symfony 5</p>
-                        <p className='text-xs'>Intégration selon maquette</p>
-                    </div>
-                    <div className=' exp-container exp-mob animate__animated' >
-                        <p className="text-sm font-bold">09/21 - 12/21</p>
-                    </div>
-                    <div className=" exp-container exp-mob animate__animated" >
-                        <p className="text-sm font-bold">Groupe QERYS (CDI)</p>
-                        <p className='text-xs'>Intégration selon maquette</p>
-                        <p className='text-xs'>Gestion API sur Symfony</p>
-                    </div>
-                    <div className=' exp-container exp-mob animate__animated' >
-                        <p className="text-sm font-bold">03/23 - 11/23</p>
-                    </div>
-                    <div className=" exp-container exp-mob animate__animated">
-                        <p className="text-sm font-bold">AIO (contrat pro + CDD)</p>
-                        <p className='text-xs'>Intégration selon maquette en Vue.js</p>
-                        <p className='text-xs'>Gestion de projet en bureau d’étude </p>
-                        <p className='text-xs'>Développement d’applications internes <br></br>(Python, Node-red, Docker)</p>
-                    </div>
-
-                </div>
+            <div className='flex margin-mob'>
                 <div className="flex flex-col items-center">
                     <div className="circle circle-mob "></div>
                     <div className="line line-mob "></div>
@@ -85,36 +51,63 @@ export default function ExpPrMobile() {
                     <div className="line line-mob "></div>
                     <div className="circle circle-mob "></div>
                 </div>
-                <div className='margin-col-3 pl-6'>
-                    <div className='  exp-container exp-mob animate__animated' >
-                        <p className="text-sm font-bold">2010 - 2021</p>
-                    </div>
-                    <div className="  exp-container exp-mob animate__animated" >
-                        <p className="text-sm font-bold">Projets Wild Code School Bordeaux</p>
-                        <p className='text-xs'>Site e-commerce  B to C pour une jeune start-up (Symfony)</p>
-                        <p className='text-xs'>Plateforme de jeux en simple MVC avec utilisation API REST</p>
-                    </div>
-                    <div className=' exp-container exp-mob animate__animated' >
-                        <p className="text-sm font-bold">08/21 - 09/21</p>
-                    </div>
-                    <div className=" exp-container exp-mob animate__animated" >
-                        <p className="text-sm font-bold">WDM studio (stage)</p>
-                        <p className='text-xs'>Intégration selon maquette</p>
-                        <p className='text-xs'>Gestion API sur Symfony</p>
-                    </div>
-                    <div className=' exp-container exp-mob animate__animated' >
-                        <p className="text-sm font-bold">03/22 - 06/22</p>
-                    </div>
-                    <div className=" exp-container exp-mob animate__animated" >
-                        <p className="text-sm font-bold">Projet Wid Code School Bordeaux</p>
-                        <p className='text-xs'>Blog interactif avec version mobile <br></br>(React / React Native)</p>
-                        <p className='text-xs'>Déploiement et intégration continue <br></br> (Docker / Github Actions)</p>
-                    </div>
-                    <div className=' exp-container exp-mob animate__animated' >
-                        <p className="text-sm font-bold">03/23 - Actuellement en poste</p>
+                <div className='flex margin-mob'>
+                    <div className='pr-4'>
+                        <div className=" exp-container exp-mob animate__animated" >
+                            <p className="text-sm font-bold">Sous officier dans l’armée de l'air :<br></br> 2010 - 2021</p>
+                            <ul className='text-xs list-disc ml-6'>
+                                <li>Chef d’équipe (Sous-officier supérieur)</li>
+                                <li>Technicien matériel servitude aéronautique</li>
+                                <li>Missions de sécurité intérieure <br></br>(Vigipirate / Cuirasse)</li>
+                            </ul>
+                        </div>
+                        <div className=" exp-container exp-mob animate__animated" >
+                            <p className="text-sm font-bold">Projets Wild Code School Bordeaux : <br></br>02/21 - 07/21</p>
+                            <ul className='text-xs list-disc ml-6'>
+                                <li className='text-xs'>Site e-commerce  B to C pour une jeune start-up (Symfony)</li>
+                                <li className='text-xs'>Intégration selon maquette</li>
+                            </ul>
+                        </div>
+                        <div className=' exp-container exp-mob animate__animated' >
+                            <p className="text-sm font-bold">Plateforme e-sport (bénévolat) :<br></br> 08/21 - 09/21</p>
+                            <ul className='text-xs list-disc ml-6'>
+                                <li className='text-xs'>Migration projet MVC vers Symfony 5</li>
+                                <li className='text-xs'>Intégration selon maquette</li>
+                            </ul>
+                        </div>
+                        <div className=" exp-container exp-mob animate__animated" >
+                            <p className="text-sm font-bold">WDM studio  (stage) : <br></br>09/21 - 12/21</p>
+                            <ul className='text-xs list-disc ml-6'>
+                                <li className='text-xs'>Intégration selon maquette</li>
+                                <li className='text-xs'>Gestion API sur Symfony</li>
+                            </ul>
+                        </div>
+                        <div className=' exp-container exp-mob animate__animated' >
+                            <p className="text-sm font-bold">Groupe QERYS (CDI) : <br></br>03/22 - 06/22</p>
+                            <ul className='text-xs list-disc ml-6'>
+                                <li className='text-xs'>Maintenance applications internes (Symfony, Zend)</li>
+                                <li className='text-xs'>Collaboration en méthodes agiles</li>
+                                <li className='text-xs'>Montée en compétences Devops <br></br>(Docker, Jenkins)</li>
+                            </ul>
+                        </div>
+                        <div className=" exp-container exp-mob animate__animated">
+                            <p className="text-sm font-bold">Projet Wid Code School Bordeaux : <br></br>03/23 - 11/23</p>
+                            <ul className='text-xs list-disc ml-6'>
+                                <li className='text-xs'>Blog interactif avec version mobile <br></br>(React / React Native)</li>
+                                <li className='text-xs'>Déploiement et intégration continue <br></br>(Docker / Github Actions)</li>
+                            </ul>
+                        </div>
+                        <div className=" exp-container exp-mob animate__animated">
+                            <p className="text-sm font-bold">AIO (contrat pro + CDD) : <br></br>03/23 - actuellement en poste</p>
+                            <ul className='text-xs list-disc ml-6'>
+                                <li className='text-xs'>Intégration selon maquette en Vue.js</li>
+                                <li className='text-xs'>Gestion de projet en bureau d’étude </li>
+                                <li className='text-xs'>Développement d’applications internes <br></br>(Python, Node-red, Docker)</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-   );
+    );
 }
